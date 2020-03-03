4f4dda139feebc8a4781a8abf0b0bd233c618d91
Kai <kergin@ucsc.edu>
2020-03-03 17:52:39 -0500

    Changed critic to environment, then added a real critic to train along side the actor. UNTESTED CODE

--- gamefeed.py
+++ gamefeed.py
@@ -5,60 +5,23 @@ from tetris import Tetris
 from PIL import Image
 import tensorflow as tf
 
-bot = Tetris(False, True)
-TRAIN_CRITIC = False
+# This file is for testing the capture card and for gathering data to train the environment
 
-BUF_SIZE = 5 # How many images to capture on each input
+bot = Tetris(False, True)
+TRAIN_ENVIRONMENT = False
+
+BUF_SIZE = 5 # How many images to capture on each input of keystroke
 IM_WIDTH = 160
 IM_HEIGHT = 90
 
 
-class _Getch:
-    """Gets a single character from standard input.  Does not echo to the
-screen."""
-    def __init__(self):
-        try:
-            self.impl = _GetchWindows()
-        except ImportError:
-            self.impl = _GetchUnix()
-
-    def __call__(self): return self.impl()
-
-
-class _GetchUnix:
-    def __init__(self):
-        import tty, sys
-
-    def __call__(self):
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
-
-class _GetchWindows:
-    def __init__(self):
-        import msvcrt
-
-    def __call__(self):
-        import msvcrt
-        return msvcrt.getch()
-
-
-getch = _Getch()
-
-def show_webcam(mirror=False):
+def show_webcam():
     cam = cv2.VideoCapture(0)
     it = 0
     while True:       
         
         # Display to screen (720p)
-        if not TRAIN_CRITIC:
+        if not TRAIN_ENVIRONMENT:
             ret_val, img = cam.read()
             im = cv2.resize(img, (IM_WIDTH,IM_HEIGHT))
             im = Image.fromarray(im)
@@ -69,99 +32,43 @@ def show_webcam(mirror=False):
                 img_data = tf.io.read_file('temp.png')
                 im = tf.image.decode_png(img_data, channels=3)
                 im = tf.reshape(tf.image.convert_image_dtype(im, tf.float64), (1,90,160,3))
-                print(np.round(bot.eval_critic(im), 2))
-                '''
-                val = bot.eval_critic(im)
-                if np.argmax(val) == 0:
-                    print("Good move")
-                elif np.argmax(val) == 1:
-                    print("Bad move")
-                else:
-                    print("neutral")
-                '''
+                print(np.round(bot.eval_environment(im), 2))
+                
             img = cv2.resize(img, (1280,720))
             cv2.imshow('Switch Display', img)
             val = cv2.waitKey(1)
             if val == 27:
                 break  # esc to quit
     
-        if TRAIN_CRITIC:
-            RENDER = True
-            if RENDER:
-                buf = []
-                val = 0
-                for _ in range(BUF_SIZE):
-                    ret_val, im = cam.read()
-                    img = cv2.resize(im, (1280,720))
-                    cv2.imshow('Switch Display', img)
-                    im = cv2.resize(im, (IM_WIDTH,IM_HEIGHT))
-                    buf.append(im)
-                    val = cv2.waitKey(1)
+        if TRAIN_ENVIRONMENT:
+            buf = []
+            val = 0
+            for _ in range(BUF_SIZE):
+                ret_val, im = cam.read()
+                img = cv2.resize(im, (1280,720))
+                cv2.imshow('Switch Display', img)
+                im = cv2.resize(im, (IM_WIDTH,IM_HEIGHT))
+                buf.append(im)
+                val = cv2.waitKey(1)
 
-                if val == 27:
-                    break
-                '''
-                Temporarily removing positive feedback
-                elif val == 32:
-                    #print("Current prediction:", bot.eval_critic(im))
-                    print("+ Giving positive feedback") # space bar
-                    #bot.fit_critic(im, np.array([0.99]))
-                    for x in buf:
-                        im = Image.fromarray(x)
-                        im.save('training/tetris/positive/' + str(random.randrange(2147483647)) + '.png')
-                '''
-                if val == 48:
-                    #print("Current prediction:", bot.eval_critic(im))
-                    print("- Giving negative feedback")
-                    #bot.fit_critic(im, np.array([-0.99]))
-                    for x in buf:
-                        im = Image.fromarray(x)
-                        im.save('training/tetris_critic/negative/' + str(random.randrange(2147483647)) + '.png')
-                elif val == 43:
-                    #print("Current prediction:", bot.eval_critic(im))
-                    print("  Neutral feedback")
-                    #bot.fit_critic(im, np.array([0.0]))
-                    for x in buf:
-                        im = Image.fromarray(x)
-                        im.save('training/tetris_critic/neutral/' + str(random.randrange(2147483647)) + '.png')
-
-            else:
-                val = getch()
-                buf = []
-                for _ in range(BUF_SIZE):
-                    ret_val, im = cam.read()
-                    im = cv2.resize(im, (IM_WIDTH,IM_HEIGHT))
-                    buf.append(im)
-
-                if val == b'q':
-                    break
-                elif val == b' ':
-                    #print("Current prediction:", bot.eval_critic(im))
-                    print("+ Giving positive feedback") # space bar
-                    #bot.fit_critic(im, np.array([0.99]))
-                    for x in buf:
-                        im = Image.fromarray(x)
-                        im.save('training/tetris/positive/' + str(random.randrange(2147483647)) + '.png')
-                elif val == b'0':
-                    #print("Current prediction:", bot.eval_critic(im))
-                    print("- Giving negative feedback") # q
-                    #bot.fit_critic(im, np.array([-0.99]))
-                    for x in buf:
-                        im = Image.fromarray(x)
-                        im.save('training/tetris/negative/' + str(random.randrange(2147483647)) + '.png')
-                else:
-                    #print("Current prediction:", bot.eval_critic(im))
-                    print("  Neutral feedback")
-                    #bot.fit_critic(im, np.array([0.0]))
-                    for x in buf:
-                        im = Image.fromarray(x)
-                        im.save('training/tetris/neutral/' + str(random.randrange(2147483647)) + '.png')
+            if val == 27:
+                break
+            elif val == 48:
+                print("- Saving negative feedback")
+                for x in buf:
+                    im = Image.fromarray(x)
+                    im.save('training/tetris_critic/negative/' + str(random.randrange(2147483647)) + '.png')
+            elif val == 43:
+                print("  Saving neutral feedback")
+                for x in buf:
+                    im = Image.fromarray(x)
+                    im.save('training/tetris_critic/neutral/' + str(random.randrange(2147483647)) + '.png')
         it += 1
     cv2.destroyAllWindows()
 
 
 def main():
-    show_webcam(mirror=False)
+    show_webcam()
 
 
 if __name__ == '__main__':
--- tetris.py
+++ tetris.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 from tensorflow.keras import Input, Model
-from tensorflow.keras.losses import MeanAbsoluteError
 from PIL import Image
 import pathlib
 import numpy as np
@@ -15,21 +14,25 @@ IM_WIDTH = 160
 IM_HEIGHT = 90
 
 SHORT_TERM_BUF = 10
-MAX_GAMES_SAVED = 5
+EVAL_FRAMES = 3
 
-OUTPUT_CRITIC = 2
 OUTPUT_ACTOR = 9
+OUTPUT_CRITIC = 1
+OUTPUT_ENVIRONMENT = 2
 
 BATCH_SIZE = 1
-EPOCHS = SHORT_TERM_BUF - 1
 
-checkpoint_path = "tetris/actor/actor.ckpt"
+GAMMA = .99999
+
+checkpoint_path_actor = "tetris/actor/actor.ckpt"
 checkpoint_path_critic = "tetris/critic/critic.ckpt"
+checkpoint_path_enviornment = "tetris/environment/environment.ckpt"
+
 
 # Callback for setting checkpoints
 # Will resave weights every 5 epochs
-cp_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath=checkpoint_path,
+cp_callback_actor = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_path_actor,
         verbose=1,
         save_weights_only=True,
         period=1)
@@ -40,20 +43,28 @@ cp_callback_critic = tf.keras.callbacks.ModelCheckpoint(
         save_weights_only=True,
         period=1)
 
+cp_callback_environment = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_path_enviornment,
+        verbose=1,
+        save_weights_only=True,
+        period=1)
+
 class Tetris:
-    def __init__(self, actor, critic):
-        self.previous_moves = tf.zeros((1,SHORT_TERM_BUF,OUTPUT_ACTOR))
-        self.previous_inputs = tf.zeros((1,SHORT_TERM_BUF,IM_HEIGHT,IM_WIDTH,3))
-        #self.internal_state = tf.zeros(20)
-        self.random = tf.random_uniform_initializer(minval=0.0, maxval=1.0)
-        self.internal_clock = 0
-        self.current_save_path = 'training/tetris_actor/game'
+    def __init__(self, actor, environment):
         if actor:
+            self.previous_moves = tf.zeros((1,SHORT_TERM_BUF,OUTPUT_ACTOR))
+            self.previous_inputs = tf.zeros((1,SHORT_TERM_BUF,IM_HEIGHT,IM_WIDTH,3))
+            self.random = tf.random_uniform_initializer(minval=0.0, maxval=1.0)
+            self.internal_clock = 0
+            self.current_save_path = 'training/tetris_actor/game'
+            self.actor_opt = tf.keras.optimizers.Adam(1e-4)
+            self.critic_opt = tf.keras.optimizers.Adam(1e-4)
             self.actor = self.__create_model_actor()
-            print("Successfully created actor")
-        if critic:
             self.critic = self.__create_model_critic()
-            print("Successfully created critic")
+            print("Successfully created actor and critic")
+        if environment:
+            self.environment = self.__create_model_environment()
+            print("Successfully created environment")
 
     def __create_model_actor(self):
         image_input = Input(shape=(SHORT_TERM_BUF,IM_HEIGHT,IM_WIDTH,3))
@@ -71,16 +82,38 @@ class Tetris:
 
         model.summary()
 
+        '''
+        No longer compiling in order to create my own loss function
         model.compile(optimizer='adam',
                       loss='binary_crossentropy',
                       metrics=['accuracy'])
+        '''
         try:
-            model.load_weights(checkpoint_path)
+            model.load_weights(checkpoint_path_actor)
         except:
             print('Error loading weights from file')
         return model
-
+    
     def __create_model_critic(self):
+        model = models.Sequential()
+        model.add(layers.Conv3D(60, (2, 5, 5), activation='relu', input_shape=(EVAL_FRAMES,IM_HEIGHT,IM_WIDTH,3)))
+        model.add(layers.AveragePooling3D((1, 3, 3)))
+        model.add(layers.Conv3D(30, (2, 5, 5), activation='relu'))
+
+        model.add(layers.Flatten())
+        model.add(layers.Dense(120, activation='relu'))
+        model.add(layers.Dense(OUTPUT_CRITIC, activation='sigmoid'))
+
+        model.summary()
+
+        try:
+            model.load_weights(checkpoint_path_critic)
+        except:
+            print('Error loading weights from file')
+
+        return model
+
+    def __create_model_environment(self):
         model = models.Sequential()
         model.add(layers.Conv2D(60, (5, 5), activation='relu', input_shape=(IM_HEIGHT,IM_WIDTH,3)))
         model.add(layers.AveragePooling2D((3, 3)))
@@ -88,7 +121,7 @@ class Tetris:
 
         model.add(layers.Flatten())
         model.add(layers.Dense(120, activation='relu'))
-        model.add(layers.Dense(OUTPUT_CRITIC, activation='softmax'))
+        model.add(layers.Dense(OUTPUT_ENVIRONMENT, activation='softmax'))
 
         model.summary()
 
@@ -96,7 +129,7 @@ class Tetris:
                       loss='binary_crossentropy',
                       metrics=['accuracy'])
         try:
-            model.load_weights(checkpoint_path_critic)
+            model.load_weights(checkpoint_path_enviornment)
         except:
             print('Error loading weights from file')
         return model
@@ -117,67 +150,108 @@ class Tetris:
         random_tensor = self.random(shape=(1,OUTPUT_ACTOR))
         # Compare > to get which moves fired
         result = tf.math.greater(output, random_tensor)
-        result_input = tf.cast(tf.equal(tf.reshape(result, (1,1,OUTPUT_ACTOR)), True), tf.float32)
+        # Convert from boolean tensor to float tensor
+        result_input_tensor = tf.cast(tf.equal(tf.reshape(result, (1,1,OUTPUT_ACTOR)), True), tf.float32)
+        result_input_file = tf.cast(tf.equal(tf.reshape(result, (1,OUTPUT_ACTOR)), True), tf.float32).numpy()
+        # Load move file
+        try:
+            move_tensor = np.load(self.current_save_path + '/0.npy')
+            move_tensor = np.concatenate((move_tensor, result_input_file), axis=0)
+        except FileNotFoundError:
+            move_tensor = result_input_file
         # Roll move buffer
         self.previous_moves = tf.slice(self.previous_moves, (0,0,0), (1,SHORT_TERM_BUF-1,OUTPUT_ACTOR))
-        self.previous_moves = tf.concat([result_input, self.previous_moves], axis=1)
+        self.previous_moves = tf.concat([result_input_tensor, self.previous_moves], axis=1)
         # Save move for current frame
-        np.save(self.current_save_path + '/' + str(self.internal_clock) + '.npy', result_input.numpy())
+        np.save(self.current_save_path + '/0.npy', move_tensor)
         # Update clock
         self.internal_clock += 1
         # Return chosen moves
         return result.numpy()
 
     # This function is used to determine if a game of tetris was lost
-    def eval_critic(self, image):
-        return self.critic(image).numpy()
+    def eval_environment(self, image):
+        return self.environment(image).numpy()
 
-    def fit_critic(self, input_generator, spe, epochs=100):
-        self.critic.fit(input_generator, 
+    def fit_enviornment(self, input_generator, spe, epochs=100):
+        self.environment.fit(input_generator, 
                     epochs = epochs,
                     verbose = 1,
                     steps_per_epoch = spe,
-                    callbacks = [cp_callback_critic])
+                    callbacks = [cp_callback_environment])
     
-    # This function will be called after a game of tetris is lost
-    def fit_actor(self):
+    def __build_dataset(self):
+        if self.dataset_built:
+            return
+        self.dataset_built = True
         data_path = pathlib.Path(self.current_save_path)
-        list_train_ds = tf.data.Dataset.list_files(str(data_path/'*.png'), shuffle=False)
-        # Each timestamp has a corresponding move file, made at the same timestamp
-        list_train_moves_ds = tf.data.Dataset.list_files(str(data_path/'*.npy'), shuffle=False)
-        image_count = len(list(data_path.glob("*.png")))
+        self.image_count = len(list(data_path.glob("*.png")))
+        if image_count < 100:
+            print("Game too short, cannot build dataset")
+            self.dataset_built = False
+            return
+        self.cur_index = image_count - (EVAL_FRAMES + 2) # start from the back of the dataset
+        self.cur_discount = -1 * GAMMA
+        self.move_tensor = np.load(self.current_save_path + '/0.npy')
+    
+    def __get_next_batch(self):
+        def build_path(name):
+            return self.current_save_path + "/" + name + ".png"
         def process_path(file_path):
             img = tf.io.read_file(file_path)
             img = tf.image.decode_png(img)
+            img = tf.expand_dims(img, axis=0)
             return img
-        train_ds = list_train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
-        train_move_ds = list_train_moves_ds.map(np.load, num_parallel_calls=AUTOTUNE)
-        def prepare_for_training(skip, ds):
-            # Uses skip to offset data for each epoch so that model is training on different predictions
-            ds.skip(skip)
-            # Batches into frames of 10
-            ds.batch(SHORT_TERM_BUF, drop_remainder=True)
-            # Reverse because neural network takes in from most recent first
-            a = lambda x: tf.reverse(x, 0)
-            ds.map(a, num_parallel_calls=AUTOTUNE)
-            return ds
-        for x in range(EPOCHS):
-            # Images must be offset by +1 due to moves being saved after image on same timestep
-            ds_images = prepare_for_training(x+1, train_ds)
-            ds_moves = prepare_for_training(x, train_move_ds)
-            ds = tf.data.Dataset.zip(ds_moves, ds_images)
-            # ASSIGN LABELS HERE
-            # TO DO
-            ds.batch(BATCH_SIZE)
-            #self.actor.fit(ds, epochs=1)
+        images = []
+        for x in range(SHORT_TERM_BUF):
+            path = build_path(str(self.cur_index - x))
+            images.append(process_path(path))
+        image_buf = tf.concat(images, axis=0)
+        image_buf = tf.expand_dims(image_buf, axis=0)
+        # Get the resulting frames from doing move
+        images = []
+        for x in range(EVAL_FRAMES):
+            path = build_path(str(self.cur_index + x))
+            images.append(process_path(path))
+        result_buf = tf.concat(images, axis=0)
+        move_chosen = self.move_tensor[self.cur_index]
+        move_buf = self.move_tensor[self.cur_index-(SHORT_TERM_BUF+1):self.cur_index]
+        move_buf = tf.convert_to_tensor(move_buf)
+        move_buf = tf.expand_dims(move_buf, axis=0)
+        self.cur_discount *= GAMMA
+        return image_buf, move_buf, move_chosen, result_buf
 
-    def reset_actor(self):
-        self.previous_moves = tf.zeros((SHORT_TERM_BUF,OUTPUT_ACTOR))
-        self.previous_inputs = tf.zeros((SHORT_TERM_BUF,IM_HEIGHT,IM_WIDTH,3))
+    # This function will be called after a game of tetris is lost
+    def fit_actor(self):
+        self.__build_dataset() 
+        while (self.cur_index-(SHORT_TERM_BUF+1) > 0):
+            batch = self.__get_next_batch()
+            train_step(batch)
+            self.cur_index -= EVAL_FRAMES
+        self.__reset_actor()
+
+    def __reset_actor(self):
+        self.previous_moves = tf.zeros((1,SHORT_TERM_BUF,OUTPUT_ACTOR))
+        self.previous_inputs = tf.zeros((1,SHORT_TERM_BUF,IM_HEIGHT,IM_WIDTH,3))
         self.internal_clock = 0
-        self.actor.reset_states()
+        self.dataset_built = False
         # Clear folder
         for filename in os.listdir(self.current_save_path):
             file_path = os.path.join(self.current_save_path, filename)
             os.unlink(file_path)
+    
+    @tf.function
+    def train_step(self, batch):
+        image_buf, move_buf, move_chosen, result_buf = batch
+        with tf.GradientTape() as act_tape, tf.GradientTape() as crit_tape:
+            probs = self.actor((image_buf, move_buf), training=True)
+            reward = self.critic(result_buf, training=True)
+            advantage = (self.cur_discount + 1) - reward
+            log_probs = tf.math.log(probs)
+            actor_loss = -1 * log_probs * advantage
+            critic_loss = 0.5 * tf.pow(advantage,2)
+        gradients_of_actor = act_tape.gradient(actor_loss, self.actor.trainable_variables)
+        gradients_of_critic = crit_tape.gradient(critic_loss, self.critic.trainable_variables)
+        self.actor_opt.apply_gradients(zip(gradients_of_actor, self.actor.trainable_variables))
+        self.critic_opt.apply_gradients(zip(gradients_of_critic, self.critic.trainable_variables))
 
